eea8dba261814c51378a1e4d282a3874345bf76e
iaraedelstein <edeliaru@gmail.com>
2021-08-14 15:50:57 -0300

    invite fix script

--- list.php
+++ list.php
@@ -1,9 +1,4 @@
-<?php
-$list = json_decode(file_get_contents("invites.json"));
-for($i = 0;$i < count($list);$i++){
-    echo '<p class="lista">'. $list[$i]->name."  TEMPLO: ".$list[$i]->templo." FIESTA: ".$list[$i]->fiesta."</p>";
-}
-?>
+
 <style>
     .lista:nth-child(odd){
         background:#ced4da;
@@ -11,3 +6,40 @@ for($i = 0;$i < count($list);$i++){
         padding:10px;
     }
 </style>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BIANCA Y GUIDO</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <table style="width: -webkit-fill-available;">
+            <thead>
+                <tr>
+                    <td>NOMBRE</td>
+                    <td>TEMPLO</td>
+                    <td>FIESTA</td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $list = json_decode(file_get_contents("invites.json"));
+                for($i = 0;$i < count($list);$i++){
+                    echo '<tr class="lista"> <td>'. $list[$i]->name."</td><td>".$list[$i]->templo."</td><td>".$list[$i]->fiesta."<td></tr>";
+                }
+            ?>
+            </tbody>
+        </table>
+    </body>
+</html>
--- upload.php
+++ upload.php
@@ -1,13 +1,12 @@
 <?php
 if($_POST){
-    $json = file_get_contents("invites.json");
-    $invites = json_decode($json);
     $invite["name"] = $_POST["name"];
     $invite["templo"] = $_POST["templo"];
     $invite["fiesta"] = $_POST["fiesta"];
-    $invites[] = $invite;
-    file_put_contents("invites.json",json_encode($invites));
-    //var_dump($invites);
+    $inp = file_get_contents('invites.json');
+    $tempArray = json_decode($inp);
+    array_push($tempArray, $invite);
+    file_put_contents('invites.json', json_encode($tempArray));
 }
 ?>
 <!DOCTYPE html>
@@ -24,10 +23,11 @@ if($_POST){
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com"> 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     </head>
-<header class="py-5 bg-image-full" style="background-image: url('./assets/img/bianguido.jpg')">
+    <body>
+        <header class="py-5 bg-image-full" style="background-image: url('./assets/img/bianguido.jpg')">
             <div class="text-center my-5">
                 <h1 class="text-white fs-3 fw-bolder">BIANCA & GUIDO</h1>
                 <p class="text-white mb-0">02-09-2021</p>
@@ -41,4 +41,5 @@ if($_POST){
         <div class="py-5 bg-image-full" style="background-image: url('./assets/img/bianguido2.jpg')">
             <div style="height: 20rem"></div>
         </div>
+    </body>
 </html>
